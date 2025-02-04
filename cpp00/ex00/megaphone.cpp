32fd0c2ec940630ab75d368c335ba40cc1735277
Yao Kai-Yuan <ykai-yua@z1t5c1.42bangkok.com>
2025-02-04 18:55:06 +0700

    tounstd

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykai-yua <ykai-yua@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 18:44:33 by ykai-yua          #+#    #+#             */
+/*   Updated: 2025/02/04 18:53:01 by ykai-yua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string str = argv[i];
+			for (size_t j = 0; j < str.length(); j++)
+				std::cout << static_cast<char>(std::toupper(str[j]));
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
