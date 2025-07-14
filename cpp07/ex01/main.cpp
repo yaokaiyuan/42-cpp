ab325943ebb8a850a4065fa022edd0a72f99b965
Yao Kai-Yuan <ykai-yua@z3t8c4.42bangkok.com>
2025-07-14 13:35:47 +0700

    non-const test added'

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ykai-yua <ykai-yua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 16:41:43 by ykai-yua          #+#    #+#             */
-/*   Updated: 2025/07/07 17:18:49 by ykai-yua         ###   ########.fr       */
+/*   Updated: 2025/07/14 13:32:20 by ykai-yua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,15 @@ std::ostream &operator<<(std::ostream &o, Awesome const &rhs) { o << rhs.get(); 
 
 template <typename T>
 void print(T const &x) { std::cout << "local print:"<< x << std::endl; return; }
+template <typename T>
+void increment(T &x) { x++; return; }
 
 int main() {
     int tab[] = { 0, 1, 2, 3, 4 };
     Awesome tab2[5];
     ::iter(tab, 5, ::print<int>);
     ::iter(tab2, 5, ::print<Awesome>);
+    ::iter(tab, 5, ::increment<int>);
+    ::iter(tab, 5, ::print<int>);
     return 0;
 }
